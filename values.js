c297595496e510d727bc7dba400ac4a2ea0ac25b
Giselle Koo <gisellegk@gmail.com>
2017-01-01 15:36:09 -0500

    add some starter values in a js array

---
+++ values.js
@@ -0,0 +1,10 @@
+values =
+[
+  "STEM",
+  "Scholarships - Apply, apply, apply!",
+  "Someone mentions Water Game",
+  "Past Game piece in Kickoff stream",
+  "Havabanana Productions",
+  "Dean Kamen is wearing denim",
+  "Woodie Flowers in a steampunk costume"
+];
