d59409f571fb7c6e9a7b319b928e7c8b1b2ae0ec
Giselle <gisellegk@users.noreply.github.com>
2017-01-01 23:04:22 -0500

    Update values.js

--- values.js
+++ values.js
@@ -9,5 +9,16 @@ values =
   "Woodie Flowers in a steampunk costume",
   "Gracious Professionalism",
   "Dean's Homework",
-  "Encryption Password is 1337"
+  "Encryption Password is 1337",
+  "Scripted role-play with steampunk theme",
+  "Scramble to download the game manual",
+  "Dozer is in the game animation",
+  "Someone is sad because Dozer's not in the game animation",
+  "One of the animation robots has a plunger",
+  "The team eats a meal together",
+  "Someone in the room tweets and ends up in the livestream",
+  "Former game pieces show up",
+  "Someone references previous year's game",
+  "Sponsor animation loops more than once",
+  "Rule update before the end of the day"  
 ];
