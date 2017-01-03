8fddf6b0a6b406292b07eb036a94e85c68d6de52
Giselle Koo <gisellegk@gmail.com>
2017-01-02 19:36:51 -0500

    i need something to send to ppl and a separate file is nicer. can merge files at the end

---
+++ values.js
@@ -0,0 +1,23 @@
+values =
+[
+  "STEM",
+  "Scholarships - Apply, apply, apply!",
+  "Someone mentions Water Game",
+  "Past Game piece in Kickoff stream",
+  "Havabanana Productions",
+  "Dean Kamen is wearing denim",
+  "Woodie Flowers in a steampunk costume",
+  "Gracious Professionalism",
+  "Dean's Homework",
+  "Encryption Password is 1337",
+  "Scripted role-play with steampunk theme",
+  "Scramble to download the game manual",
+  "Dozer is in the game animation",
+  "Someone is sad because Dozer's not in the game animation",
+  "One of the animation robots has a plunger",
+  "The team eats a meal together",
+  "Someone in the room tweets and ends up in the livestream",
+  "Someone references previous year's game",
+  "Sponsor animation loops more than once",
+  "Rule update before the end of the day"
+];
